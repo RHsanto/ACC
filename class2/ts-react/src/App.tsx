6c88222d8f5a22e1d89f2dac31ac73b9bdb2c36f
RHsanto <rakibulsanto789@gmail.com>
2022-07-14 11:45:43 +0600

    add

--- class2/ts-react/src/App.tsx
+++ class2/ts-react/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Lists from './components/Lists';
 
 function App() {
   return (
     <div className="App">
     <h1>Hello Hacker group</h1>
+    <Lists/>
     </div>
   );
 }
